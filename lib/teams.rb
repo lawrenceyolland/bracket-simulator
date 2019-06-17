617d33bb71932aea5a1dae192f7513907bc95ac6
github username <github email address>
2019-06-17 10:52:49 +0100

    added teams arrays

---
+++ lib/teams.rb
@@ -0,0 +1,23 @@
+# Atlantic Division
+tampa_bay_lightning = ["Nikita Kucherov", "Steven Stamkos", "J.T. Miller", "Anthony Cirelli", "Erik Cernak"]
+boston_bruins = ["Brad Marchand", "Zdeno Chara", "David Pastrnak", "Patrice Bergeron", "Charlie McAvoy"]
+toronto_maple_leafs = ["Auston Matthews", "John Taveres", "Mitch Marner", "William Nylander", "Patrick Marleau"]
+# Metropolitan Division
+washington_capitals = ["Alexander Ovechkin", "Tom Wilson", "T.J. Oshie", "Nicklas Backstrom", "Evgeny Kuznetsov"]
+new_york_islanders = ["Jordan Eberle", "Matthew Barzal", "Josh Bailey", "Devon Toews", "Brock Nelson"]
+pittsburgh_penguins = ["Sidney Crosby", "Phil Kessel", "Jake Guentzel", "Evgeni Malkin", "Kris Letang"]
+#Wildcard
+carolina_huricanes = ["Teuvo Teravainen", "Sebastian Aho", "Warren Foegele", "Jordan Staal", "Justin Williams"]
+columbus_blue_jackets = ["Artemi Panarin", "Matt Duchene", "Seth Jones", "Cam Atkinson", "Zach Werenski"]
+
+# Central Division
+nashville_predators = ["P.K. Subban", "Roman Josi", "Ryan Ellis", "Ryan Johansen", "Filip Forsberg"]
+winnipeg_jets = ["Blake Wheeler", "Mark Scheifele", "Kyle Connor", "Patrik Laine", "Dustin Byfuglien"]
+st_louis_blues = ["Ryan O'Reilly", "Jaden Schwartz", "Alex Pietrangelo", "Vladimir Tarasenko", "David Perron"]
+# Pacific Division
+calgary_flames = ["Johnny Gaudreau", "Sean Monohan", "Matthew Tkachuk", "Elias Lindholm", "Mark Giordano"]
+san_jose_sharks = ["Erik Karlsson", "Brent Burns", "Logan Couture", "Evander Kane", "Joe Thornton"]
+las_vegas_golden_knights = ["Johnathan Marchessault", "Mark Stone", "Max Pacioretty", "William Karlsson", "Reilly Smith"]
+#Wildcard
+dallas_stars = ["Tyler Seguin", "Mats Zuccarello", "Alexander Radulov", "Jamie Benn", "John Klingberg"]
+colorado_avalanche = ["Mikko Rantanen", "Nathan MacKinnon", "Gabriel Landeskog", "Cale Makar", "Patrick Roy"]

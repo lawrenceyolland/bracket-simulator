be7311618a726d10fa0db5d916031d5f8372efb0
github username <github email address>
2019-06-19 10:15:04 +0100

    modified score assigner

--- lib/score_assigner.rb
+++ lib/score_assigner.rb
@@ -1,20 +1,19 @@
 require "pry"
-example_csv = ["6-1","3-2","1-2","5-1","4-5","4-3"]
+score = ["6-1","3-2","1-2","5-1","4-5","4-3"]
 
 def give_goals(player_selector, goal_assigner)
     puts "player#{player_selector} scored #{goal_assigner} goals"
 end
 
-game_goals = example_csv.sum{|s| s[0].to_i}
-current_goals = game_goals 
-puts game_goals
-until current_goals == 0
-    player_selector = rand(4)
-    goal_assigner = rand(1..current_goals)
-    current_goals = current_goals - goal_assigner
-    give_goals(player_selector, goal_assigner)
+def assign_goals(score)
+    current_goals = score.sum{|s| s[0].to_i}
+    until current_goals == 0
+        player_selector = rand(4)
+        goal_assigner = rand(1..current_goals)
+        current_goals = current_goals - goal_assigner
+        give_goals(player_selector, goal_assigner)
+    end
 end
-binding.pry
-# 
+
 
 
